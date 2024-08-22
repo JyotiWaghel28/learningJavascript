5d0c8a61f0d986785d02032fd29f89e4d2b5dfa5
jyoti <wagheljyoti2899@gmail.com>
2024-08-22 11:49:15 +0530

    jsbasics

---
+++ basics/variables.js
@@ -0,0 +1,18 @@
+const accountId=133
+let email="wagheljyotid@gmail.com"
+var accountpass="12355"
+accountcity="Agra"
+let accountstate;
+let nulltype=null;
+
+////only use const and var
+/*
+prefer not to use var
+because of issues in block scope and functional scope
+*/
+//accountId=87
+email="uiojuojo"
+accountpass= "1235555"
+accountcity="iuiop"
+console.log(12355);
+console.table([accountId,email,accountpass,accountcity,accountstate,nulltype])
